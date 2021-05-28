42c4574c100141ce7a32fbbb79f69453bb8745d2
aliasgar1978 <aholo2000@gmail.com>
2021-05-28 01:09:55 -0700

    version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="capture_it",
-    version="0.0.2",
+    version="0.0.3",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="Excel Facts File Generate from Networking Device Output|Config",
