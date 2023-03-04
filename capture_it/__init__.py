6e3822211558051b9e618f95a3ef4e0fa460e800
= <=>
2023-03-04 18:56:11 +0530

    ver14initialpush

--- capture_it/__init__.py
+++ capture_it/__init__.py
@@ -2,6 +2,6 @@ __doc__ = '''Facts Capture'''
 
 __all__ = [ 'Execute_By_Login' ]
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 from .executions import Execute_By_Login as capture
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ docs_extras = [
 
 setuptools.setup(
     name="capture_it",
-    version="0.0.13",
+    version="0.0.14",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="Excel Facts File Generate from Networking Device Output|Config",
