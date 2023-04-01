b1ca80996cb0d2093aad4df76541d3cb46c1cf5d
aliasgar1978 <aholo2000@gmail.com>
2023-04-01 08:25:46 +0530

    ver0015 init

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ docs_extras = [
 
 setuptools.setup(
     name="capture_it",
-    version="0.0.14",
+    version="0.0.15",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="Excel Facts File Generate from Networking Device Output|Config",
