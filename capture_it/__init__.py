5038390febf65bc753436f69c1cac3ec6e777e73
aliasgar1978 <aholo2000@gmail.com>
2023-10-30 18:22:12 +0530

    .

--- capture_it/__init__.py
+++ capture_it/__init__.py
@@ -1,6 +1,6 @@
 __doc__ = '''Device Output Capture Utility'''
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .executions import Execute_By_Login as capture
 from .executions import Execute_By_Individual_Commands as capture_individual
